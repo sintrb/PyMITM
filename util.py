31b680931b4423b35e7b334e22817c9532e54ac1
RobinTang <sintrb@gmail.com>
2014-07-04 16:45:47 +0800

    utils

--- util.py
+++ util.py
@@ -5,23 +5,6 @@
 # @Link    : https://github.com/sintrb/PyMITM
 # @Version : 1.0
 
-def recvall(soc):
-	buflen = 65535
-	data = []
-	while True:
-		try:
-			d = soc.recv(buflen)
-			data.append(d)
-		except:
-			d = ''
-		if len(d) < buflen:
-			break
-		soc.settimeout(5)
-	return ''.join(data)
-		# if 'Content-Encoding' in headers and headers['Content-Encoding'] == 'gzip':
-		# 	da = StringIO.StringIO(self.databody)
-		# 	gf = gzip.GzipFile(fileobj=da)
-		# 	self.databody = gf.read()
-		# 	gf.close()
-		# 	da.close()
-		# 	del headers['Content-Encoding']
+def getselfidp():
+	import socket
+	return socket.gethostbyname(socket.gethostname())
